c50957e432304b22b3a5df1626b160c655179c26
Johanne Eide Omland <johanneeideomland@live.no>
2023-02-18 15:37:50 +0100

    Fix BookCard bug #29

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <></>;
-}
+'use client';
+import { Grid, Card, Text, Container } from '@nextui-org/react';
+import React from 'react';
+import BookCard from './components/BookCard';
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1>Welcome to my homepage</h1>
+      <BookCard title="Lord of the Rings" rating={5} />
+    </div>
+  );
+};
+
+export default Homepage;
