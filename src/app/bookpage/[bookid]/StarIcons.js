56cd8acaf49394dfd2332277de1ef026ec970048
Ulrik Isdahl <ulrikisdahl@Ulriks-MacBook-Air.local>
2023-02-18 21:43:46 +0100

    Add icon and style to bookpage

--- src/app/bookpage/[bookid]/StarIcons.js
+++ src/app/bookpage/[bookid]/StarIcons.js
@@ -31,6 +31,10 @@ const useStyles = makeStyles((theme) => ({
   },
   btn: {
     marginLeft: '13px',
+    backgroundColor: '#22b573',
+  },
+  saveBtn: {
+    marginLeft: '13px',
   },
 }));
 
@@ -88,7 +92,7 @@ function StarIcons() {
           </Button>
         ) : (
           <Button
-            className={classes.btn}
+            className={classes.saveBtn}
             variant="contained"
             color="primary"
             onClick={handleSaveClick}
@@ -102,3 +106,102 @@ function StarIcons() {
 }
 
 export default StarIcons;
+
+// import React, { useState } from 'react';
+// import { makeStyles } from '@material-ui/core/styles';
+// import StarIcon from '@material-ui/icons/Star';
+// import StarBorderIcon from '@material-ui/icons/StarBorder';
+// import Typography from '@material-ui/core/Typography';
+
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     display: 'flex',
+//     alignItems: 'center',
+//   },
+//   stars: {
+//     display: 'flex',
+//     alignItems: 'center',
+//     marginBottom: theme.spacing(1),
+//   },
+//   star: {
+//     color: theme.palette.secondary.main,
+//     fontSize: 50,
+//     marginRight: theme.spacing(1),
+//     cursor: 'pointer',
+//   },
+//   starEmpty: {
+//     color: theme.palette.grey[400],
+//     fontSize: 50,
+//     marginRight: theme.spacing(1),
+//     cursor: 'pointer',
+//   },
+//   text: {
+//     fontSize: 20,
+//   },
+// }));
+
+// function StarIcons() {
+//   const classes = useStyles();
+//   const initialRating = 4;
+//   const [rating, setRating] = useState(initialRating);
+//   const [tempRating, setTempRating] = useState(initialRating);
+//   const [rated, setRated] = useState(false);
+
+//   const handleMouseOver = (value) => {
+//     setTempRating(value);
+//   };
+
+//   const handleClick = (value) => {
+//     setRating(value);
+//     setRated(true);
+//   };
+
+//   const handleMouseLeave = () => {
+//     if (!rated) {
+//       setTempRating(initialRating);
+//     }
+//   };
+
+//   const handleSaveClick = () => {
+//     setRated(true);
+//   };
+
+//   return (
+//     <>
+//       <div className={classes.root}>
+//         <div
+//           className={classes.stars}
+//           onMouseLeave={handleMouseLeave}
+//           onClick={handleSaveClick}
+//         >
+//           {[1, 2, 3, 4, 5].map((value) => {
+//             const star =
+//               value <= (rated ? rating : tempRating) ? (
+//                 <StarIcon
+//                   className={classes.star}
+//                   key={value}
+//                   onMouseOver={() => handleMouseOver(value)}
+//                   onClick={() => handleClick(value)}
+//                 />
+//               ) : (
+//                 <StarBorderIcon
+//                   className={classes.starEmpty}
+//                   key={value}
+//                   onMouseOver={() => handleMouseOver(value)}
+//                   onClick={() => handleClick(value)}
+//                 />
+//               );
+//             return star;
+//           })}
+//         </div>
+//         <Typography variant="body1" className={classes.text}>
+//           {rated
+//             ? `You have rated this book ${rating} stars.`
+//             : `Please rate this book (hover over stars to rate).`}
+//         </Typography>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default StarIcons;
--- src/app/bookpage/[bookid]/page.js
+++ src/app/bookpage/[bookid]/page.js
@@ -5,10 +5,30 @@ import StarIcons from './StarIcons';
 import { useEffect, useState } from 'react';
 
 import PocketBase from 'pocketbase';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import ClearIcon from '@mui/icons-material/Clear';
+
+import { useRouter } from 'next/navigation';
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    position: 'relative',
+    top: 0,
+    left: 0,
+    backgroundColor: '#22b573',
+  },
+}));
 
 const pb = new PocketBase('http://127.0.0.1:8090');
 
 function Book({ params }) {
+  const router = useRouter();
+
+  const classes = useStyles();
+
   const [records, setRecords] = useState([]);
 
   useEffect(() => {
@@ -29,9 +49,15 @@ function Book({ params }) {
     foo();
   }, []);
 
+  // <h1 id="title">Book page {params.bookid}</h1>
   return (
     <>
-      <h1 id="title">Book page {params.bookid}</h1>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <div></div>
+        <IconButton color="primary" onClick={() => router.back()}>
+          <ClearIcon style={{ fontSize: 40 }} />
+        </IconButton>
+      </div>
       <div id="gridContainer">
         <div className="item tall">
           <img src={img.src} alt="book cover"></img>
@@ -43,9 +69,17 @@ function Book({ params }) {
         <div className="item">
           <StarIcons></StarIcons>
         </div>
-        <div className="item">di</div>
         <div className="item">
-          {records.length > 0 && records[0].description}
+          <Button
+            className={classes.button}
+            variant="contained"
+            color="primary"
+          >
+            Add to my library
+          </Button>
+        </div>
+        <div className="item">
+          <p>{records.length > 0 && records[0].description}</p>
         </div>
         <div className="item">
           <p className="book-text">
