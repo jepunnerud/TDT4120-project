3837e9b357e90352c48c212effaeb721861e28ef
Jakob Eilertsen Punnerud <jakobep@stud.ntnu.no>
2023-04-30 15:20:06 +0200

    Remove footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,7 @@ export default function RootLayout({ children }) {
       <body>
         <NavBar />
         {children}
-        <Footer />
+        {/* <Footer /> */}
       </body>
     </html>
   );
